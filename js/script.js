cc6a8b6c7d3be58a193df9a59a6922c0908f8423
sengu11 <sen_gu@naver.com>
2023-01-11 17:41:05 +0900

    완성하기

---
+++ js/script.js
@@ -0,0 +1,130 @@
+
+  $("#search").focusin(function(){
+    $(".input-box__placeholder label").addClass("focus");
+  });
+  
+  $("#search").focusout(function(){
+    $(".input-box__placeholder label").removeClass("focus");
+    $(this).val("");
+  });
+  
+
+  
+  $("#mobile-search").focusin(function(){
+    $(".input-box__placeholder label").addClass("focus");
+  });
+  
+  $("#mobile-search").focusout(function(){
+    $(".input-box__placeholder label").removeClass("focus");
+    $(this).val("");
+  });
+  
+
+  
+  $("#search").focusin(function(){
+    $(".input-box__placeholder label").addClass("focus");
+  });
+  
+  $("#search").focusout(function(){
+    $(".input-box__placeholder label").removeClass("focus");
+    $(this).val("");
+  });
+  
+  
+  
+  function SliderBox1__init() {
+    const swiper = new Swiper(".slider-box-1 .swiper", {
+      // Optional parameters
+      loop: true,
+
+      autoplay:{
+        delay:5000,
+        disableOnInteraction : false,
+        },
+
+      // If we need pagination
+      pagination: {
+        el: ".slider-box-1 .swiper-pagination"
+      },
+  
+      // Navigation arrows
+      navigation: {
+        nextEl: ".slider-box-1 .swiper-button-next",
+        prevEl: ".slider-box-1 .swiper-button-prev"
+      }
+    });
+  }
+  
+  SliderBox1__init();
+  
+
+
+  function Rank__init() {
+    const swiper = new Swiper(".rank .swiper", {
+      direction:'vertical',
+      
+      // Optional parameters
+      loop: true,
+  
+      autoplay:{
+      delay:2500,
+      disableOnInteraction : false,
+      },
+
+    });
+  }
+  
+  Rank__init();
+  
+
+
+  $('.btn span').click(function(){
+    $('.btn span').removeClass('active');
+    $(this).addClass('active');
+
+    var i = $(this).parent().index();
+    $('.bwrap > section').fadeOut(0);
+    $('.bwrap > section').eq(i).fadeIn(0);
+  });
+
+
+
+  $(".more-rank").click(function(){
+    $(this).toggleClass("down");
+  });
+
+  $(".more-rank").click(function(){
+    $(".rank__full-menu").stop().fadeToggle(300);
+  });
+
+
+
+  var $window = $(window);
+
+  $window.scroll(function(){
+    var scrollTop = $window.scrollTop();
+
+    if ( scrollTop > 0 ){
+      $(".header__top").addClass("active");
+    }
+
+    else{
+      $(".header__top").removeClass("active");
+    }
+  });
+
+  $('.menu-icon').click(function(){
+    $('.full-menu').show();
+  ;});
+
+  // $(document).mouseup(function (e) {
+    
+  //   var container = $(".full-menu");
+    
+  //   if (!container.is(e.target) && container.has(e.target).length === 0){
+    
+  //   container.css("display","none");
+    
+  //   }	
+    
+  //   });
